fcf6d26cc23a8458e17b19bfff9052d2b0ba39a5
HoangDuongTT9 <33827197+HoangDuongTT9@users.noreply.github.com>
2026-03-04 10:09:40 +0700

    Update osint-phone-lookup-secure.meta.js

--- osint-phone-lookup-secure.meta.js
+++ osint-phone-lookup-secure.meta.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         OSINT Phone Lookup Tool (Secure Edition)
 // @namespace    http://tampermonkey.net/
-// @version      2.0.0
-// @description  Advanced OSINT tool with auto-update and remote kill-switch
+// @version      2.1.0
+// @description  Advanced OSINT tool with auto-update and remote kill-switch + VietQR Automation
 // @author       Your Name
 // @match        https://www.google.com/*
 // @match        about:blank
 // @match        https://uidphone.xyz/*
 // @match        https://www.facebook.com/*
 // @match        https://chat.zalo.me/*
+// @match        https://vietqr.net/*
 // ==/UserScript==
